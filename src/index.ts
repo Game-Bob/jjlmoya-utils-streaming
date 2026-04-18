6ee8c234b455d548f3494996ea556198a801d690
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:56 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,6 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { SorteoComponent, SorteoSEO, SorteoBibliography } from './tool/sorteo';
 export { SORTEO_TOOL } from './tool/sorteo/index';
 
-export { TebasCheckComponent, TebasCheckSEO, TebasCheckBibliography } from './tool/tebasCheck';
 export { TEBAS_CHECK_TOOL } from './tool/tebasCheck/index';
